bc16e19ad3640ce770ea8aa56e7b7358cbd86119
Ali Raza <aliraza137725@gmail.com>
2023-10-04 16:20:00 +0500

    some changes are done

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -3,7 +3,6 @@ import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
   UploadOutlined,
-  UserOutlined,
   VideoCameraOutlined,
 } from '@ant-design/icons';
 import { Layout, Menu, Button, theme } from 'antd';
@@ -11,86 +10,47 @@ import { Home } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 const { Header, Sider, Content } = Layout;
 const App = () => {
-  const [collapsed, setCollapsed] = useState(false);
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
+
   const getRandomId = () => Math.random().toString(36).slice(2)
   return (
-    <Layout >
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="demo-logo-vertical" />
-        <Menu
-          theme="light"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          items={[
+    <>
 
-            {
-              key: getRandomId,
-              icon: <Home style={{fontSize:"23px"}}/>,
-              label: <Link to='/'></Link>,
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
-        />
-      </Sider>
-      <Layout>
-        <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
-          }}
-        >
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: '16px',
-              width: 64,
-              height: 64,
-            }}
-          />
-        </Header>
-        <Content
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            minHeight: 280,
-            background: colorBgContainer,
-          }}
-        >
-          Content
-        </Content>
-      </Layout>
-    </Layout>
+      items={[
+        {
+          key: getRandomId,
+          icon: <Home style={{ fontSize: "23px" }} />,
+          label: <Link to='/'></Link>,
+        },
+        {
+          key: '2',
+          icon: <VideoCameraOutlined />,
+          label: 'nav 2',
+        },
+        {
+          key: '2',
+          icon: <VideoCameraOutlined />,
+          label: 'nav 2',
+        },
+        {
+          key: '2',
+          icon: <VideoCameraOutlined />,
+          label: 'nav 2',
+        },
+        {
+          key: '2',
+          icon: <VideoCameraOutlined />,
+          label: 'nav 2',
+        },
+        {
+          key: '3',
+          icon: <UploadOutlined />,
+          label: 'nav 3',
+        }
+      ]}
+
+    </>
   );
 };
 export default App;
-    
+
 
--- src/Pages/Frontend/index.js
+++ src/Pages/Frontend/index.js
@@ -1,10 +1,53 @@
-import React from 'react'
-import Sidebar from '../../Components/Sidebar/Sidebar'
-
+import React, { useContext, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 export default function index() {
+    const [collapsed, setCollapsed] = useState(false);
+    const { dispatch } = useContext(AuthContext);
+    const navigate = useNavigate()
+    // const {
+    //   token: { colorBgContainer },
+    // } = theme.useToken();
     return (
         <>
-            <Sidebar />
+            <Layout >
+                <Sider trigger={null} collapsible collapsed={collapsed}>
+                    <div className="demo-logo-vertical" />
+                    <Menu
+                        theme="light" mode="inline" style={{backgroundColor:"red",color:"black"}} defaultSelectedKeys={['1']}
+
+                    />
+                </Sider>
+                <Layout>
+                    <Header
+                        style={{
+                            padding: 0,
+                            background: colorBgContainer,
+                        }}
+                    >
+                        <Button
+                            type="text"
+                            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                            onClick={() => setCollapsed(!collapsed)}
+                            style={{
+                                fontSize: '16px',
+                                width: 64,
+                                height: 64,
+                            }}
+                        />
+                    </Header>
+                    <Content
+                        style={{
+                            margin: '24px 16px',
+                            padding: 24,
+                            minHeight: 280,
+                            background: colorBgContainer,
+                        }}
+                    >
+                        Content
+                    </Content>
+                </Layout>
+            </Layout>
+
         </>
     )
 }
