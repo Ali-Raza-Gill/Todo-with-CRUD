b686f3f1d4ed210ccebbbc7bd96f31066e800d80
Ali Raza <aliraza137725@gmail.com>
2023-10-04 15:08:04 +0500

    Side-bar is added to this project

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,37 +1,96 @@
-import React from 'react'
-import "./style.scss"
-import HomeIcon from '@mui/icons-material/Home';
-import PersonSearchIcon from '@mui/icons-material/PersonSearch';
-import CallIcon from '@mui/icons-material/Call';
-import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
-import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
-export default function Sidebar() {
+import React, { useState } from 'react';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu, Button, theme } from 'antd';
+import { Home } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+const { Header, Sider, Content } = Layout;
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  const getRandomId = () => Math.random().toString(36).slice(2)
   return (
-    <div className="sidebar">
-      <div className="row">
-        <div className="col-auto">
-          <ul>
-            <li>
-              <h3>Todo Sidebar</h3>
-            </li>
-            <li className=' '>
-            <a href="" className='my-5'><HomeIcon/> Home</a>
-            </li>
-            <li>
-              <a href=""><PersonSearchIcon/> About</a>
-            </li>
-            <li>
-              <a href=""><CallIcon/> Contact</a>
-            </li>
-            <li>
-              <a href=""><PlaylistAddIcon/> Todo</a>
-            </li>
-            <li>
-              <a href=""><AppRegistrationIcon/> Show Todo</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  )
-}
+    <Layout >
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="light"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+
+            {
+              key: getRandomId,
+              icon: <Home style={{fontSize:"23px"}}/>,
+              label: <Link to='/'></Link>,
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: 'nav 2',
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: 'nav 2',
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: 'nav 2',
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: 'nav 2',
+            },
+            {
+              key: '3',
+              icon: <UploadOutlined />,
+              label: 'nav 3',
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+export default App;
+    
+
--- src/Pages/Frontend/index.js
+++ src/Pages/Frontend/index.js
@@ -1,29 +1,10 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './Home/Home'
-import Contact from './Contact/Contact'
-import About from './About/About'
-import Navbar from '../../Components/Navbar/Navbar'
-import Footer from '../../Components/Footer/Footer'
-import Nopage from './Nopage'
-import Todo from './Todo/Todo'
 import Sidebar from '../../Components/Sidebar/Sidebar'
 
 export default function index() {
     return (
         <>
-            <Navbar />
-            <Sidebar/>
-            <main>
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='todo' element={<Todo />} />
-                    <Route path='contact' element={<Contact />} />
-                    <Route path='about' element={<About />} />
-                    <Route path='*' element={<Nopage />} />
-                </Routes>
-            </main>
-            <Footer />
+            <Sidebar />
         </>
     )
 }
