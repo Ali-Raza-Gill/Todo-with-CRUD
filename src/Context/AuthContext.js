d54ab9f849e05124170f24d585de808ef6f75c2a
Ali Raza <aliraza137725@gmail.com>
2023-10-06 12:49:07 +0500

    some more code is written in Authcontext page

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { createContext, useEffect, useReducer } from 'react'
 
-export default function AuthContext() {
-  return (
-    <div>AuthContext</div>
-  )
-}
+
+export const AuthContext = createContext()
+const initialState = { isAuthenticated: false }
+
+const reducer = ((state, action) => {
+    switch(state.type){
+case "LOGIN":
+return{isAuthenticated:true, user:action.payload.user}
+case "LOGOUT":
+    return{isAuthenticated:false}
+    default:
+        return(state)
+    }
+})
+
+
+    export default function AuthContextProvider(props) {
+    const [state, dispatch] = useReducer(reducer, initialState) 
+
+    useEffect(()=>{
+onAuthStateChanged(Auth,(user)=>{
+    if(user){
+        console.log("user is log in successfully")
+        dispatch({type:"LOGIN",payload:{user}})
+    }
+})
+
+    },[])
+
+        return (
+            <div>AuthContext</div>
+        )
+    }
