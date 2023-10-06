3b7ba755fe6507cee5db4bda4acc9799e4c9e38c
Ali Raza <aliraza137725@gmail.com>
2023-10-06 13:15:26 +0500

    some modificaitons are dome

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -1,35 +1,41 @@
 import React, { createContext, useEffect, useReducer } from 'react'
-
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from '../config/firebase'
 
 export const AuthContext = createContext()
 const initialState = { isAuthenticated: false }
 
 const reducer = ((state, action) => {
-    switch(state.type){
-case "LOGIN":
-return{isAuthenticated:true, user:action.payload.user}
-case "LOGOUT":
-    return{isAuthenticated:false}
-    default:
-        return(state)
+    switch (state.type) {
+        case "LOGIN":
+            return { isAuthenticated: true, user: action.payload.user }
+        case "LOGOUT":
+            return { isAuthenticated: false }
+        default:
+            return (state)
     }
 })
 
 
-    export default function AuthContextProvider(props) {
-    const [state, dispatch] = useReducer(reducer, initialState) 
+export default function AuthContextProvider(props) {
+    const [state, dispatch] = useReducer(reducer, initialState)
 
-    useEffect(()=>{
-onAuthStateChanged(Auth,(user)=>{
-    if(user){
-        console.log("user is log in successfully")
-        dispatch({type:"LOGIN",payload:{user}})
-    }
-})
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                console.log("user is log in successfully")
+                dispatch({ type: "LOGIN", payload: { user } })
+            } else {
+                dispatch({ type: "LOGOUT" })
+                console.log("User in signed out now")
+            }
+        })
 
-    },[])
+    }, [])
 
-        return (
-            <div>AuthContext</div>
-        )
-    }
+    return (
+        <AuthContext.Provider value={{ ...state, dispatch }}>
+            {props.childern}
+        </AuthContext.Provider>
+    )
+}
--- src/Pages/Routes.js
+++ src/Pages/Routes.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Frontend from './Frontend'
 import Auth from './Auth'
+import { AuthContext } from '../Context/AuthContext'
+
+
 export default function Index() {
+const {isAuthenticated} = useContext(AuthContext)
+
   return (<>
     <Routes>
-      <Route path='/*' element={<Frontend />} />
-      <Route path='/auth/*' element={<Auth />} />
+      <Route path='/*' element={<privateRoute component={<Frontend />}/>} />
+      <Route path='/auth/*' element={!isAuthenticated? <Auth />: <Navigate to="/"/>} />
       {/* <Route path='/dashboard' element={<DashBoard/>}/> */}
     </Routes>
     
